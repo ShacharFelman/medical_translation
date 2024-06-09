bd33c2e6cc497ceba846f717438affe549628e6d
ShacharFelman <Shachar.Felman@s.afeka.ac.il>
2024-06-09 23:47:21 +0300

    start of refactoring route and error_handlers

--- flask_backend/error_handlers.py
+++ flask_backend/error_handlers.py
@@ -2,7 +2,11 @@ from flask import jsonify
 from logger import logger
 
 
-def handle_invalid_user_input_error(error):
+def handle_exception(error):
+    logger.error(error)
+    return internal_server_error()
+
+def invalid_user_input_error(error):
     return jsonify({
         'error': {
             "eng": str(error.english_message),
@@ -10,11 +14,34 @@ def handle_invalid_user_input_error(error):
         }
     }), 400
 
-def handle_exception(error):
-    logger.error(error)
+def internal_server_error():
     return jsonify({
         'error': {
             "eng": 'internal server error',
-            "heb": "בעיה בסרבר"
+            "heb": "בעיה בשרת"
         }
     }), 500
+
+def missing_data_in_request_error():
+    return jsonify({'error': {
+        "eng":"missing data in request",
+        "heb":"בעיה בעיבוד הבקשה, חסרים נתונים"
+        }}), 400
+
+def unsupported_source_language_error():
+        return jsonify({'error': {
+            "eng": "source language is not supported",
+            "heb": "שפת התרגום לא קיימת"
+        }}), 400
+
+def unsupported_dest_language_error():
+        return jsonify({'error': {
+            "eng": "dest language is not supported",
+            "heb": "שפה המוצא לא קיימת"
+        }}), 400
+    
+def engine_not_initialized_response():
+    return jsonify({'error': {
+        "eng": 'translation engine is still loading. Please wait...',
+        "heb": "מודל התרגום בטעינה, אנא המתן..."
+    }}), 503
--- flask_backend/routes/services.py
+++ flask_backend/routes/services.py
@@ -1,6 +1,6 @@
 from flask import jsonify, request ,Blueprint
 from logger import logger
-from exceptions import InvalidUserInputError
+from error_handlers import engine_not_initialized_response, missing_data_in_request_error, unsupported_source_language_error, unsupported_dest_language_error, internal_server_error
 from engine.engine import translation_engine
 from constants import Language
 from flask import send_file
@@ -23,73 +23,27 @@ def create_user_error(error_in_english:str,error_in_hebrew:str,status_code:int=5
         "heb":error_in_hebrew
     }}),status_code
 
-def create_interval_error():
-    return jsonify({"error":{
-        "eng":"interval server error",
-        "heb":"בעיה בסרבר"
-    }}),500
-
-
-
-@services_bp.route('/reference', methods=['POST'])
-def upload_reference_file():    
-    file = request.files.get('file')
-    if not file:
-        return jsonify({'error': {
-            "eng":"file not found",
-            "heb":"קובץ לא נמצא"
-            }}), 404    
-    
-    if file.filename == '':
-        return jsonify({'error': {
-            "eng":'no selected file',
-            "heb":"קובץ לא נבחר"
-        }}), 400    
-    
-    if not file.filename.endswith(".docx") and not file.filename.endswith(".doc"):
-        raise InvalidUserInputError("file type is  not supported, please upload a word document","קובץ אינו נתמך אנא השתמש בקובץ וורד")
-
-
-    file_text = get_word_document_text(file)
-
-    # upload reference file after proccesing
-    file_data = file_proccessor.process(file.filename,file_text)
-    reference_token = file_cache.register_file(file.filename,file_text,file_data)
-    # return cache token 
-    return jsonify({'data':{"reference_token": reference_token}}), 200
-
 
 @services_bp.route('/text', methods=['POST'])
 def translate_text():    
     if not translation_engine.is_initialized():
-        return jsonify({'error': {
-            "eng":'translation engine is still loading. Please wait...',
-            "heb":"מודל התרגום בטעינה, אנא המתן..."
-            }}), 503    
+        return engine_not_initialized_response()   
     
     try:        
         data = request.json
-        # reference_token = data['referenceToken']
         source = data['source']
         dest = data['dest']
         text_input = data['textInput']    
         html_input = data['htmlInput']
     except Exception as e:
         logger.error(e)
-        return jsonify({'error': {
-            "eng":"missing data in request",
-            "heb":"בעיה בעיבוד הבקשה, חסרים נתונים"
-            }}), 400
+        return missing_data_in_request_error()
+    
     languages = [lang.value for lang in Language]
     if source not in languages:
-        return jsonify({'error': {
-            "eng":"source language is not supported",
-            "heb":"שפת התרגום לא קיימת"}}), 400
+        return unsupported_source_language_error()
     if dest not in languages:
-        return jsonify({'error': {
-            "eng":"dest language is not supported",
-            "heb":"שפה המוצא לא קיימת"
-            }}), 400
+        return unsupported_dest_language_error()
 
     errors = input_validation_handler.get_errors(text_input)
     if len(errors) > 0:
@@ -103,7 +57,7 @@ def translate_text():
     if len(errors) > 0:
         for error in errors:
             logger.error(error)
-        return create_interval_error()
+        return internal_server_error()
 
     return jsonify({'data': output_text}), 200
 
