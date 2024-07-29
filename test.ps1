7c58badc45a42af856662781f224adad2bcfa69e
ShacharFelman <Shachar.Felman@s.afeka.ac.il>
2024-07-29 12:27:32 +0300

    Changed test.ps1 - Now you can decide which test to run by specify the test file name in the running command. Added docker-compose file for testing without mongo, to use it - use the flag '-nomongo' when running the test.ps1

--- test.ps1
+++ test.ps1
@@ -1,5 +1,33 @@
+param(
+    [Parameter(ValueFromRemainingArguments = $true)]
+    [string[]]$TestNames,
+    [switch]$nomongo
+)
+
+$TestsToRun = if ($TestNames) {
+    ($TestNames | ForEach-Object { "tests.$_" }) -join ','
+}
+else {
+    "all"
+}
+
+if ($TestsToRun -eq "all") {
+    Write-Host "No specific tests specified. Running all tests."
+}
+else {
+    Write-Host "Running tests: $TestsToRun"
+}
+
+$ComposeFile = if ($nomongo) {
+    "docker-compose.nomongo.test.yml"
+}
+else {
+    "docker-compose.test.yml"
+}
+
 try {
-    docker-compose -f docker-compose.test.yml up --build
+    $env:TESTS_TO_RUN = $TestsToRun
+    docker-compose -f $ComposeFile up --build
 }
 finally {
     docker image prune -f
